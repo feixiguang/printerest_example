36550b0ad92e1a616cc428a8dfed6fe18366eed6
feixiguang <sfxg27@gmail.com>
2017-07-17 21:59:13 +0800

    update pins controller create and layouts to haml

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,6 +9,12 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to pins_path, notice: "Successfully created new Pin"
+    else
+      render :new
+    end
   end
 
   private
